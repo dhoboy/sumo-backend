1115f8fe66ba3ae00cbe90f18ec14678448e6683
dhoboy <dhoverstreet@gmail.com>
2022-08-15 01:44:51 +0900

    Adding rikishi_test.clj

---
+++ test/sumo_backend/data/rikishi_test.clj
@@ -0,0 +1,37 @@
+(ns sumo-backend.data.rikishi-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [sumo-backend.data.rikishi :refer [get-rikishi
+                                               list-rikishi
+                                               rikishi-exists?]]))
+
+
+(deftest rikishi
+  (testing "get-rikishi"
+    (is (=
+          "ENDO"
+          (:name (first (get-rikishi "endo"))))
+      "Returns record for passed in rikishi")
+    (is (=
+          '(:id :name :image :name_ja)
+          (keys (first (get-rikishi "endo"))))
+      "Returns record with :id, :name, :image, :name_ja keys")
+    (is (=
+          '()
+          (get-rikishi "not a rikishi"))
+      "Returns an empty list if rikishi record does not exist"))
+
+  (testing "list-rikishi"
+    (is (=
+          '(:id :name :image :name_ja)
+          (keys (first (list-rikishi))))
+      "Returns records with :id, :name, :image, :name_ja keys"))
+
+  (testing "rikishi-exists?"
+    (is (=
+          false
+          (rikishi-exists? "Sam"))
+      "Invalid Rikishi name returns false")
+    (is (=
+          true
+          (rikishi-exists? "Endo"))
+      "Valid Rikishi name returns true")))
